90cf3d74d63e073fe05c928f4105f3c3ea1a9a89
yahyal0v3 <yahyal0v3@yahoo.com>
2018-09-04 20:44:14 -0700

    Set up index and App react js files

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+ 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+ 
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>, document.getElementById('container')
+)
 registerServiceWorker();
